6b1b271ab61d11ea2f7985efdf3806506d4096f2
dharanish <dharanishwar.2006@gmail.com>
2025-03-23 09:21:39 -1200

    changes made

--- public/js/chat.js
+++ public/js/chat.js
@@ -106,7 +106,6 @@ chatNamespace.on("online", (data) => {
   data.forEach((user) => {
     if (roomNumber == user.roomNumber) {
       onlineUsers.innerHTML += `
-      <p class = 'RoomName'>${user.roomNumber}</p>
             <li>
             <button type="button" class="btn btn-light mx-2 p-2" data-toggle="modal" data-target="#pvChat" data-id=${
               user.id
